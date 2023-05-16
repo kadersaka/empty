9c945acbb1e411c1a5335a5d35b34a6ddf844e8b
Xampy <xampydev@gmail.com>
2023-05-16 20:35:47 +0000

    update: clean import

--- lib/views/faq/faq_page.dart
+++ lib/views/faq/faq_page.dart
@@ -1,21 +1,13 @@
-import 'package:empty/widget/text/text_header_one_widget.dart';
 import 'package:flutter/material.dart';
 
-import '../../widget/DrawerWidget.dart';
 import '../../widget/accordion/custom_accordion_widget.dart';
-import '../../widget/appBar/default_app_bar_widget.dart';
-import '../../widget/appBar/top_header_menu_bar.widget.dart';
 import '../../widget/container/page_container_widget.dart';
-import '../application_view.dart';
 
 class FaqPage extends StatelessWidget {
-  FaqPage({super.key});
-
-  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+  const FaqPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    ApplicationView.globalKey = _globalKey;
 
     return PageContainerWidget(
         appBarTitle: "FAQ's",
