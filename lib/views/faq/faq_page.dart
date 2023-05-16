a86eaf856dc0c60ea8cca8a3235e197b81f12f41
Xampy <xampydev@gmail.com>
2023-05-16 20:32:39 +0000

    update: create container for pages

--- lib/views/faq/faq_page.dart
+++ lib/views/faq/faq_page.dart
@@ -5,6 +5,7 @@ import '../../widget/DrawerWidget.dart';
 import '../../widget/accordion/custom_accordion_widget.dart';
 import '../../widget/appBar/default_app_bar_widget.dart';
 import '../../widget/appBar/top_header_menu_bar.widget.dart';
+import '../../widget/container/page_container_widget.dart';
 import '../application_view.dart';
 
 class FaqPage extends StatelessWidget {
@@ -12,53 +13,20 @@ class FaqPage extends StatelessWidget {
 
   final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
 
-  final List<Map<String, dynamic>> _items = List.generate(
-      20,
-      (index) => {
-            'id': index,
-            'title': 'Item $index',
-            'description':
-                'This is the description of the item $index. There is nothing important here. In fact, it is meaningless.',
-          });
-
   @override
   Widget build(BuildContext context) {
     ApplicationView.globalKey = _globalKey;
 
-    return Scaffold(
-        endDrawer: SearchingProfileDrawerWidget(),
-        drawerEnableOpenDragGesture: true,
-        endDrawerEnableOpenDragGesture: true,
-        key: _globalKey,
-        body: SafeArea(
-            child: Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(20.0),
-                child: Column(children: [
-                  DefaultAppBarWidget(
-                    dark: true,
-                    globalKey: _globalKey,
-                  ),
-                  TopHeaderMenuBar(
-                      color: Colors.black,
-                      title: "FAQ's",
-                      onBack: () {
-                        Navigator.pop(context);
-                      }),
-                  Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          const Accordion(
-                            title: 'What is YuTU',
-                            content:
-                                "YuTU is a local social video platform where people help people and lift each other up. It's a place to build local communities that are inclusive and progressive",
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ]))));
+    return PageContainerWidget(
+        appBarTitle: "FAQ's",
+        child: const Column(
+          children: [
+            Accordion(
+              title: 'What is YuTU',
+              content:
+                  "YuTU is a local social video platform where people help people and lift each other up. It's a place to build local communities that are inclusive and progressive",
+            ),
+          ],
+        ));
   }
 }
---
+++ lib/widget/container/page_container_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../views/application_view.dart';
+import '../DrawerWidget.dart';
+import '../appBar/default_app_bar_widget.dart';
+import '../appBar/top_header_menu_bar.widget.dart';
+
+class PageContainerWidget extends StatelessWidget {
+  final Widget child;
+  final String appBarTitle;
+  PageContainerWidget({super.key, required this.child, required this.appBarTitle});
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    ApplicationView.globalKey = _globalKey;
+
+    return Scaffold(
+        endDrawer: SearchingProfileDrawerWidget(),
+        drawerEnableOpenDragGesture: true,
+        endDrawerEnableOpenDragGesture: true,
+        key: _globalKey,
+        body: SafeArea(
+            child: Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(20.0),
+                child: Column(children: [
+                  DefaultAppBarWidget(
+                    dark: true,
+                    globalKey: _globalKey,
+                  ),
+                  TopHeaderMenuBar(
+                      color: Colors.black,
+                      title: appBarTitle,
+                      onBack: () {
+                        Navigator.pop(context);
+                      }),
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: SingleChildScrollView(
+                      child: child,
+                    ),
+                  )
+                ]))));
+  }
+}
